f3d78998ecb21561ccc7627156752d0578e9b776
Aaradhya Sharma <aaradhy0012@gmail.com>
2026-04-18 02:16:29 -0500

    Added sift_up sift_down push method to the heap.h

--- Graph.h
+++ Graph.h
@@ -1,34 +1,73 @@
-#ifndef _GRAPH_H_
-#define _GRAPH_H_
-#include <iostream>
-#include <vector>
-#include <map>
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<iostream>
+#include<vector>
+#include<list>
+#include<map>
+
 using namespace std;
 
-struct Neighbor {
-	string destination;
-	int weight;
-};
+/*
+class graph {
+	vector<vector<int> > adj_matrix;
 
-struct Edge { // struct for addEdge from a .txt file
-	string start;
-	string end;
-	int Inputweight;
+public:
+	graph(int n) {
+		adj_matrix = vector<vector<int> >(n, vector<int>(n, 0));
+	}
+
+	void addEdge(int u, int v) {
+		adj_matrix[u][v] = 1;
+
+		adj_matrix[v][u] = 1;
+		cout << "added edge " << u << " , " << v << " to the matrix" << endl;
+
+	}
+
+	void print() {
+		cout << "Adjecency Matrix for current graph" << endl;
+
+		int n = adj_matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << adj_matrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
 };
+*/
 
 class Graph {
+	map<int, list<int> >
+		adjList; // Adjacency list to store the graph
 
-	map<string, vector<Neighbor>> adjList;
-	vector<vector<int>> adj_matrix;
-	
-	public:
-		Graph();
-		void addVertex(string name); // add vertex to graph if it does not exist yet
-		void addEdge(string start, string destination, int weight); // add directed edge
-		void printVertices(); // print all vertices in the graph
-		void printEdges(); // print all edges and their weights
-		void PrintAdjacencyList();
-		vector<Neighbor> getNeighbors(string vertex); // return list of neighbors for a given vertes
-	
+public:
+	// Function to add an edge between vertices u and v of
+	// the graph
+	void addEdge(int u, int v)
+	{
+		// Add edge from u to v
+		adjList[u].push_back(v);
+		// Add edge from v to u because the graph is
+		// unidirected
+		adjList[v].push_back(u);
+	}
+
+	void print()
+	{
+		cout << "Adjacency list for the Graph: " << endl;
+		// Iterate over each vertex
+		for (auto i : adjList) {
+			// Print the vertex
+			cout << i.first << " -> ";
+			// Iterate over the connected vertices
+			for (auto j : i.second) {
+				// Print the connected vertex
+				cout << j << " ";
+			}
+			cout << endl;
+		}
+	}
 };
-#endif 
+#endif
