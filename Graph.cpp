616f023a6ac5f9cd2efa1b01de6c9391ac30ffc6
betsygwilson <elizabethgracewilson11@gmail.com>
2026-04-21 17:54:52 -0500

    Add files via upload
    added the method headers:)

--- Graph.cpp
+++ Graph.cpp
@@ -1,6 +1,21 @@
 #include "Graph.h"
 #include <vector>
+//------------------------------------------------------------
+// Method:      Graph (constructor)
+// Author:      Catherine
+// Description: Initializes an empty graph with no vertices or edges
+// Incoming:    no data
+// Outgoing:    no data
+//------------------------------------------------------------
 Graph::Graph(){}
+
+//------------------------------------------------------------
+// Method:      addVertex
+// Author:      Catherine
+// Description: Adds a new vertex to the graph if it does not already exist
+// Incoming:    namem the name of the vertex to add (string)
+// Outgoing:    no data
+//------------------------------------------------------------
 void Graph::addVertex(string name){
 	//fixing the case
 
@@ -9,6 +24,17 @@ void Graph::addVertex(string name){
 	}
 }
 
+//------------------------------------------------------------
+// Method:      addEdge
+// Author:      Catherine
+// Description: Adds a directed edge between two vertices with a given weight.
+//              normalizes capitalization of vertex names, creates vertices if
+//              they do not exist, and ignores duplicate edges
+// Incoming:    start - the source vertex (string)
+//              destination - the destination vertex (string)
+//              weight, the cost of the edge in years (int)
+// Outgoing:    no data
+//------------------------------------------------------------
 void Graph::addEdge(string start, string destination, int weight){
 	
 	//fixing the case for vertices
@@ -33,6 +59,13 @@ void Graph::addEdge(string start, string destination, int weight){
 	adjList[start].push_back(newNeighbor);
 }
 
+//------------------------------------------------------------
+// Method:      printVertices
+// Author:      Catherine
+// Description: Prints all vertices currently in the graph to the console
+// Incoming:    no data
+// Outgoing:    no data
+//------------------------------------------------------------
 void Graph::printVertices(){
 	cout << "Available Career Paths: " << endl;
 	for (const pair<string, vector<Neighbor>>& p : adjList) { // loop through adjacency list
@@ -40,6 +73,14 @@ void Graph::printVertices(){
 	}
 }
 
+//------------------------------------------------------------
+// Method:      printEdges
+// Author:      Catherine
+// Description: Prints all edges in the graph with their source, destination,
+//              and weight to the console
+// Incoming:    no data
+// Outgoing:    no data
+//------------------------------------------------------------
 void Graph::printEdges(){
 	cout << "Graph Connection with Weights: " << endl;
 	for (const pair<string, vector<Neighbor>>& p : adjList) { // loop through vertex and its neighbor list
@@ -49,6 +90,13 @@ void Graph::printEdges(){
 	}
 }
 
+//------------------------------------------------------------
+// Method:      PrintAdjacencyList
+// Author:      Catherine
+// Description: Prints the adjacency matrix of the graph to the console
+// Incoming:    none
+// Outgoing:    none
+//------------------------------------------------------------
 void Graph::PrintAdjacencyList(){
 
 	cout << "Adjacency matrix for current graph" << endl;
@@ -61,11 +109,27 @@ void Graph::PrintAdjacencyList(){
 	}
 }
 
+//------------------------------------------------------------
+// Method:      getNeighbors
+// Author:      Catherine
+// Description: Returns a list of all neighboring vertices for a given vertex.
+//              creates the vertex if it does not already exist
+// Incoming:    vertex - the name of the vertex to look up (string)
+// Outgoing:    vector of Neighbor objects connected to the given vertex
+//------------------------------------------------------------
 vector<Neighbor> Graph:: getNeighbors(string vertex){ // return list of neighbors for a given vertex
 	addVertex(vertex); // make sure vertex exists, create if missing
 	return adjList[vertex]; // return vector of neighbors
 }
 
+
+//------------------------------------------------------------
+// Method:      getVertices
+// Author:      Catherine
+// Description: Returns a list of all vertex names currently in the graph
+// Incoming:    no data
+// Outgoing:    vector of strings containing all vertex names
+//------------------------------------------------------------
 vector<string> Graph::getVertices(){
 		vector<string> vertices;
 
